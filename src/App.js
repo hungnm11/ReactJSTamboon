89ed26aa3aa1132f3a9318c13989e4656b0d6d88
Hung Nguyen <hung.nguyen@hotmail.com.vn>
2019-07-03 16:34:50 +0700

    handle Donate pay

--- src/App.js
+++ src/App.js
@@ -59,6 +59,7 @@ const Card = styled.div`
   -webkit-box-shadow: -2px 2px 5px 0px rgba(179, 179, 179, 0.58);
   -moz-box-shadow: -2px 2px 5px 0px rgba(179, 179, 179, 0.58);
   box-shadow: -2px 2px 5px 0px rgba(179, 179, 179, 0.58);
+  position: relative;
 `;
 
 const Button = styled.button`
@@ -72,6 +73,35 @@ const Button = styled.button`
   align-self: center;
   margin-left: auto;
   margin-right: 10px;
+`;
+
+const Pay = styled.button`
+  font-size: 1em;
+  padding: 10px;
+  border-radius: 3px;
+  background-color: #ffffff;
+  border: 1px solid #136bb5;
+  color: #136bb5;
+  align-self: center;
+  margin-left: 10px;
+`;
+
+const Modal = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  margin: auto;
+  background-color: rgba(255,255,255, .9);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+const Label = styled.label`
+  float: left;
 `;
 
 export default connect((state) => state)(
@@ -104,34 +134,33 @@ export default connect((state) => state)(
         })
     }
 
-    handleDonateButton = () => {
-      console.log('Donate')
-    }
-
     render() {
       const self = this;
       const cards = this.state.charities.map(function (item, i) {
         const payments = [10, 20, 50, 100, 500].map((amount, j) => (
-          <label key={j}>
+          <Label key={j}>
             <input
               type="radio"
               name="payment"
               onClick={function () {
                 self.setState({ selectedAmount: amount })
               }} /> {amount}
-          </label>
+          </Label>
         ));
-
         return (
           <Column key={i} xs="12" sm="6" md="6">
             <Card >
               <Image src="https://cdn.vaildaily.com/wp-content/uploads/sites/7/2016/09/CvrTrain-VDN-092816-1240x826.jpg" alt="" />
               <FooterItem>
                 <P>{item.name}</P>
-                <Button>Donate</Button>
+                <Button onClick={function () {
+                  self.setState({ isDonate: item.id })
+                }} >Donate</Button>
               </FooterItem>
-              {payments}
-              <button onClick={handlePay.call(self, item.id, self.state.selectedAmount, item.currency)}>Pay</button>
+              {self.state.isDonate === item.id && (<Modal>
+                <p>Select the amount to donate (USD)</p>
+              <P>{payments}</P>
+              <Pay onClick={handlePay.call(self, item.id, self.state.selectedAmount, item.currency)}>Pay</Pay></Modal>)}
             </Card>
           </Column>
         );
